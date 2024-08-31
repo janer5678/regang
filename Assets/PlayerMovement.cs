f1dbd24afa5db2163174e933ce653c3c56153c3a
biancayay <biancasantayana@gmail.com>
2024-08-31 16:52:07 +1200

    biancca character done besides jump and gun and m/s

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -16,6 +16,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform groundcheck;
 
     [SerializeField] private LayerMask groundlayer;
+    [SerializeField] private GameObject groundCheckMiddle;
+    [SerializeField] private GameObject groundCheckLeft;
+    [SerializeField] private GameObject groundCheckRight;
+    [SerializeField] private bool isGrounded = false;
+    [SerializeField] private Animator animator;
 
 
 
@@ -24,34 +29,47 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-
     void Update()
     {
-        print(IsGrounded());
+        RaycastHit2D groundHitMiddle = Physics2D.Raycast(groundCheckMiddle.transform.position, -Vector2.up, 0.1f, groundlayer);
+        RaycastHit2D groundHitLeft = Physics2D.Raycast(groundCheckLeft.transform.position, -Vector2.up, 0.1f, groundlayer);
+        RaycastHit2D groundHitRight = Physics2D.Raycast(groundCheckRight.transform.position, -Vector2.up, 0.1f, groundlayer);
+
+
+        if (groundHitMiddle || groundHitLeft || groundHitRight)
+        {
+            isGrounded = true;
+        } else
+        {
+            isGrounded = false;
+        }
+
+
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            animator.SetBool("Movement", true);
             character.transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
 
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
+            animator.SetBool("Movement", true);
             character.transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
-
-
-        }
-        if (Input.GetKeyDown(KeyCode.W) && IsGrounded())
+        } else
         {
-            rb.velocity = new Vector2(rb.velocity.x, jump_force);
-
+            animator.SetBool("Movement", false);
         }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        {
+            animator.SetBool("Jumping", true);
+            rb.velocity = new Vector2(rb.velocity.x, jump_force);
+        } else
+        {
+            animator.SetBool("Jumping", true);
+        }
+
     }
 
-
-
-
-    //&& IsGrounded())
-    private bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundcheck.position, radius, groundlayer);
-    }
 }
