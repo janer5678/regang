126ae6228abdf2199ce6c2809a398ea5dba85fec
pendevx <pendevx@gmail.com>
2024-09-07 14:31:41 +1200

    GH: playerstate

--- Assets/GH/GDMovement.cs
+++ Assets/GH/GDMovement.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class GDMovement : MonoBehaviour
 {
+    private enum PlayerState
+    {
+        Cube,
+        Ship
+    }
+
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
     private bool canJump = false;
     private bool canDrop = false;
+    private PlayerState playerState;
 
     public GameObject[] raycasts;
     public float SPEED_X = 5.0f;
@@ -16,27 +25,42 @@ public class GDMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TogglePlayerState();
+        }
+
+        if (playerState == PlayerState.Cube)
+        {
+            CubeMovement();
+        }
+        else
+        {
+            ShipMovement();
+        }
+    }
+
+    void CubeMovement()
+    {
         rb.velocity = new Vector2(0, rb.velocity.y);
 
         if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb.velocity = new Vector2(-SPEED_X, rb.velocity.y);
-        }
-
+            MoveHorizontally(-SPEED_X);
         if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rb.velocity = new Vector2(SPEED_X, rb.velocity.y);
-        }
+            MoveHorizontally(SPEED_X);
+
         if (Input.GetKey(KeyCode.UpArrow) && canJump)
         {
             canJump = false;
             canDrop = true;
             rb.velocity = new Vector2(rb.velocity.x, SPEED_Y);
         }
+
         if (Input.GetKey(KeyCode.DownArrow) && canDrop)
         {
             canDrop = false;
@@ -52,5 +76,39 @@ public class GDMovement : MonoBehaviour
 
             Debug.DrawRay(raycast.transform.position, -Vector2.up * 0.1f, Color.red);
         }
+    }
+
+    void ShipMovement()
+    {
+        
+    }
+
+    void MoveHorizontally(float speed)
+        => rb.velocity = new Vector2(speed, rb.velocity.y);
+
+    void TogglePlayerState()
+    {
+        var nextPlayerState = playerState == PlayerState.Cube ? PlayerState.Ship : PlayerState.Cube;
+        
+        if (nextPlayerState == PlayerState.Cube)
+        {
+            SwitchToCube();
+        }
+        else
+        {
+            SwitchToShip();
+        }
+    }
+
+    void SwitchToCube()
+    {
+        playerState = PlayerState.Cube;
+        Debug.Log("Switched to cube");
+    }
+
+    void SwitchToShip()
+    {
+        playerState = PlayerState.Ship;
+        Debug.Log("Switched to ship");
     }
 }
